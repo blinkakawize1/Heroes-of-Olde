08df9fa2c5c8f89cc511072994ea72f106cf2836
blinkakawize1 <43358163+blinkakawize1@users.noreply.github.com>
2021-01-31 12:25:38 -0500

    Add files via upload

---
+++ GODOT/Build001/LineEdit.gd
@@ -0,0 +1,19 @@
+extends LineEdit
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_LineEdit_text_entered(_new_text):
+	clear()
